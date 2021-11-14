8c592557cbb5759823394309d3781f141d59faaf
Lobster <Lobster_Jian_Lang@163.com>
2021-11-14 21:33:38 +0800

    This is the first front-end version

---
+++ Bilibili_Xiu_Assistant/pages/login/login.vue
@@ -0,0 +1,63 @@
+<template>
+		<view>
+			<view class="cu-form-group">
+				<view class="title">账号</view>
+				<input type="text" placeholder="请输入账号..." name="userName" 
+				@input="userNameInput" 
+				confirm-type="done" confirm-hold="true"></input>
+			</view>
+			<view class="cu-form-group">
+				<view class="title">密码</view>
+				<input type="text" placeholder="请输入密码..." name="passWord" 
+				confirm-type="done" confirm-hold="true"></input> 
+			</view>
+			<view class="padding">
+				<button class="cu-btn block bg-blue margin-tb-sm lg" @click="subBtn" type="" :disabled="isAble" >
+					<text v-if="loadFlag" class="cuIcon-loading2 cuIconfont-spin"></text>提交
+				</button>
+			</view>
+		</view>
+</template>
+
+<script>
+	export default{
+		data(){
+			return{
+				userNameInp : '',
+				userNameLen : '',
+				loadFlag : false,
+				isAble : true
+			}
+		},
+		methods:{
+			userNameInput(e){
+				// console.log(e);
+				this.userNameInp = e.target.value;
+				this.userNameLen = e.target.cursor;
+				if(this.userNameLen>0){
+					this.isAble = false
+				}else{
+					this.isAble = true
+				}
+			},
+			subBtn(e){
+				this.loadFlag = true,
+				this.isAble = true
+				var $this = this;
+				setTimeout(function(){
+					// uni.hideLoading()
+					uni.showToast({
+						title: "提交成功!"
+					});
+					$this.loadFlag = false
+					$this.isAble = false
+				},500)
+			}
+		}
+		
+	}
+
+</script>
+
+<style>
+</style>
---
+++ Bilibili_Xiu_Assistant/pages/start/start.vue
@@ -0,0 +1,59 @@
+<template>
+	<view>
+		<view class="img-f">
+			<image src="../../static/logo.png" mode="widthFix"></image>
+		</view>
+		<view class="slogan1">咻一下</view>
+		<view class="slogan2">随时随地得到最新推送</view>
+		<view class="btn-f">
+			<button size="mini" plain="true" class="btn" @tap="goToNextPage()"><text>登录进入</text></button>
+		</view>
+		
+	</view>
+</template>
+
+<script>
+	export default{
+		methods: {
+			goToNextPage(){
+				uni.navigateTo({
+			                url: '../login/login',
+			            });
+		}
+	}
+	}
+</script>
+
+<style>
+	page{
+		background-color: #000;
+	}
+	.img-f{
+		margin-top: 50rpx;
+		text-align: center;
+	}
+	image {
+	   width: 65%;
+	   margin: auto;
+	}
+	.slogan1{
+		margin-top: -20rpx;
+		color: #FFFFFF;
+		font-size: 32px;
+		text-align: center;
+	}
+	.slogan2{
+		margin-top: 30rpx;
+		color: #FFFFFF;
+		font-size: 16px;
+		text-align: center;
+	}
+	.btn-f{
+		margin-top: 100rpx;
+		text-align: center;
+	}
+	text{
+		font-size: 14px;
+		color: #FFFFFF
+	}
+</style>
